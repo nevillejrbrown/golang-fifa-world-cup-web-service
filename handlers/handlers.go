bead38ef165c6b5108c2f951651183ee612d3a44
Neville Brown <neville.brown@gmail.com>
2020-02-22 18:11:39 +0000

    implemented AddNewWinner method

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -24,7 +24,7 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 	} else {
 		filteredWinners, err := data.ListAllByYear(year)
 		if err != nil {
-			res.WriteHeader(http.StatusInternalServerError)
+			res.WriteHeader(http.StatusBadRequest)
 			return
 		}
 		res.Write(filteredWinners)
@@ -34,7 +34,18 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
-
+	accessToken := req.Header.Get("X-ACCESS-TOKEN")
+	isTokenValid := data.IsAccessTokenValid(accessToken)
+	if !isTokenValid {
+		res.WriteHeader(http.StatusUnauthorized)
+	} else {
+		err := data.AddNewWinner(req.Body)
+		if err != nil {
+			res.WriteHeader(http.StatusUnprocessableEntity)
+		} else {
+			res.WriteHeader(http.StatusCreated)
+		}
+	}
 }
 
 // WinnersHandler is the dispatcher for all /winners URL
